378e10459001dddd83ee8e55a63812723f573d9c
Ugiloy <ugiloy519@gmail.com>
2026-08-25 20:32:12 +0500

    done

--- if03.py
+++ if03.py
@@ -7,4 +7,10 @@ def main(a):
     Returns:
         a: integer
     """
-    return
+    if a >0:
+        return a+1
+    elif a <0:
+        return a-2
+    elif a ==0:
+        return 10
+print(main(6))
