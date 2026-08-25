e3dede0e2554b27d79e6d44a8c401290770cd4e6
Ugiloy <ugiloy519@gmail.com>
2026-08-25 20:32:40 +0500

    done

--- if04.py
+++ if04.py
@@ -8,4 +8,10 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    if a >0:
+        return a
+    elif b>0:
+        return b
+    elif c>0:
+        return c
+print(main(2,6,9))
